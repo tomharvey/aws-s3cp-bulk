f4d2d3392a64cfcde2822837b5c24af59dc21e74
Thomas Harvey <tom@alush.co.uk>
2017-12-28 18:06:13 -0300

    run verify with more logging

--- handler.js
+++ handler.js
@@ -11,14 +11,12 @@ module.exports.bulk_cp = (event, context, callback) => {
   console.log(event);
   console.log(context);
 
-  var lambda = new AWS.Lambda();
   var params = {
     FunctionName: 'copy',
     InvocationType: Event,
-    Payload: {
-      ['s3://src/file/1.jpg', 's3://dst/file/1.jpg'],
-      ['s3://src/file/2.jpg', 's3://dst/file/2.jpg']
-    }
+    Payload: 'tom'
   };
-  lambda.invoke(params, callback)
+
+  var lambda = new AWS.Lambda();
+  lambda.invoke(params, callback);
 }
