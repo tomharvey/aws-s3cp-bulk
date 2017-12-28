f39ad468ac0c295a47c004cefde088fcbcaaa3f6
Thomas Harvey <tom@alush.co.uk>
2017-12-28 18:16:25 -0300

    invokation type as a string

--- handler.js
+++ handler.js
@@ -11,12 +11,22 @@ module.exports.bulk_cp = (event, context, callback) => {
   console.log(event);
   console.log(context);
   console.log("Invoking")
+
   var params = {
     FunctionName: 'aws-s3cp-bulk-production-copy',
-    InvocationType: Event,
+    InvocationType: 'Event',
     Payload: 'tom'
   };
 
   var lambda = new AWS.Lambda();
-  lambda.invoke(params, callback);
+  lambda.invoke(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      callback(err, data)
+    }
+    else{
+      console.log(data);
+      callback(err, data)
+    }
+  });
 }
