a8f820ee4d97dcaad38514e631d84a452e27a974
Thomas Harvey <tom@alush.co.uk>
2017-12-28 19:06:25 -0300

    json payload

--- handler.js
+++ handler.js
@@ -18,18 +18,20 @@ module.exports.bulk_cp = (event, context, callback) => {
   var params = {
     FunctionName: 'aws-s3cp-bulk-production-copy',
     InvocationType: 'Event',
-    Payload: "tom"
+    Payload: JSON.stringify({
+      src: 'foo',
+      dst: 'bar'
+    })
   };
 
   var lambda = new AWS.Lambda();
   lambda.invoke(params, function(err, data) {
     if (err) {
       console.log(err, err.stack);
-      callback(err, data)
     }
     else{
       console.log(data);
-      callback(err, data)
     }
+    callback(err, data);
   });
 }
