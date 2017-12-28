f23e95cab45811868aa1918d02a730e42d3de588
Thomas Harvey <tom@alush.co.uk>
2017-12-28 17:47:13 -0300

    removing the hello function

--- handler.js
+++ handler.js
@@ -1,21 +1,5 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
-
 module.exports.cp = (event, context, callback) => {
   console.log(event)
   console.log(context)
