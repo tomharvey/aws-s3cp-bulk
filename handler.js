884cd3669b39ca8d8ef0c71299ffe41624ed9424
Thomas Harvey <tom@alush.co.uk>
2017-12-28 18:02:09 -0300

    run bulk copy and invoke solo copy

--- handler.js
+++ handler.js
@@ -6,3 +6,19 @@ module.exports.cp = (event, context, callback) => {
 
   callback(null, false)
 }
+
+module.exports.bulk_cp = (event, context, callback) => {
+  console.log(event);
+  console.log(context);
+
+  var lambda = new AWS.Lambda();
+  var params = {
+    FunctionName: 'copy',
+    InvocationType: Event,
+    Payload: {
+      ['s3://src/file/1.jpg', 's3://dst/file/1.jpg'],
+      ['s3://src/file/2.jpg', 's3://dst/file/2.jpg']
+    }
+  };
+  lambda.invoke(params, callback)
+}
