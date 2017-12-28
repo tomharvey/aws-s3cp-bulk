c1506634c9fdf5b81c15fcd1f612abeacbd561f5
Thomas Harvey <tom@alush.co.uk>
2017-12-28 17:44:27 -0300

    add a copy function and SNS

--- handler.js
+++ handler.js
@@ -14,3 +14,11 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+
+module.exports.cp = (event, context, callback) => {
+  console.log(event)
+  console.log(context)
+
+  callback(null, false)
+}
