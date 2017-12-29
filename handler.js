72eb500bdd1c34fe22b7fdf3875543c0ed878d16
Thomas Harvey <tom@alush.co.uk>
2017-12-29 16:14:45 -0300

    load verification module into handlers

--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@
 
 const AWS = require('aws-sdk');
 const copy = require('./lib/copy');
+const verification = require('./lib/verification');
 
 
 module.exports.cp = (event, context, callback) => {
@@ -10,8 +11,8 @@ module.exports.cp = (event, context, callback) => {
   console.log(context)
 
   const [src, dst] = verification(valid_event)
-  // if (src && dst) copy(event.src, event.dst, callback)
-  //   else callback(new Error("The source and/or destination were not valid"), event)
+  if (src && dst) copy(event.src, event.dst, callback)
+    else callback(new Error("The source and/or destination were not valid"), event)
 }
 
 module.exports.manager = (event, context, callback) => {
