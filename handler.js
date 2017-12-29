06b0035b17985f23fe6dadbdb8548bf68ce19983
Thomas Harvey <tom@alush.co.uk>
2017-12-29 16:29:49 -0300

    invoke from module

--- handler.js
+++ handler.js
@@ -3,13 +3,10 @@
 const AWS = require('aws-sdk');
 const copy = require('./lib/copy');
 const verification = require('./lib/verification');
+const invoke = require('./lib/invoke');
 
 
 module.exports.cp = (event, context, callback) => {
-  console.log("Starting")
-  console.log(event)
-  console.log(context)
-
   const [src, dst] = verification(event)
   if (src && dst) copy(event.src, event.dst, callback)
     else callback(new Error("The source and/or destination were not valid"), event)
@@ -17,44 +14,5 @@ module.exports.cp = (event, context, callback) => {
 
 module.exports.manager = (event, context, callback) => {
   console.log(event);
-  console.log(context);
-  console.log("Invoking")
-
-  var params1 = {
-    FunctionName: 'aws-s3cp-bulk-production-copy',
-    Payload: JSON.stringify({
-      src: 'foo',
-      dst: 'bar'
-    })
-  };
-
-  var params2 = {
-    FunctionName: 'aws-s3cp-bulk-production-copy',
-    Payload: JSON.stringify({
-      src: 'baz',
-      dst: 'bat'
-    })
-  };
-
-  var lambda = new AWS.Lambda();
-  
-  lambda.invoke(params1, function(err, data) {
-    console.log("Returned 1")
-    if (err) {
-      console.log(err, err.stack);
-    }
-    else{
-      console.log(data);
-    }
-  });
-
-  lambda.invoke(params2, function(err, data) {
-    console.log("Returned 2")
-    if (err) {
-      console.log(err, err.stack);
-    }
-    else{
-      console.log(data);
-    }
-  });
+  invoke('foo', 'bar', callback)
 }
---
+++ tests/invoke_test.js
@@ -0,0 +1,19 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var invoke = require('../lib/invoke');
+
+describe ('Invoke', () => {
+    it('should generate params to invoke a copy function', () => {
+        const src = 'foo'
+        const dst = 'bar'
+
+        expect(invoke.get_invoke_params(src, dst)).to.deep.equal({
+            FunctionName: 'aws-s3cp-bulk-production-copy',
+            Payload: JSON.stringify({
+                src: 'foo',
+                dst: 'bar'
+            })
+        })
+    })
+})
